6bedaa834e022d9d12b647eeda2d58b959470cf2
Peter Horvath <horvthpeter@gmail.com>
2022-02-14 00:19:24 +0200

    Prepare canvas init.

--- build/main.js
+++ build/main.js
@@ -1,5 +1,7 @@
 function initGame() {
     const canvas = document.getElementById('canvas');
+    canvas.width = window.innerWidth - 2;
+    canvas.height = window.innerHeight - 2;
     const ctx = canvas.getContext('2d');
 }
 window.addEventListener('DOMContentLoaded', () => {
--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,9 @@
 function initGame() {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+    canvas.width = window.innerWidth - 2;
+    canvas.height = window.innerHeight - 2;
+
     const ctx = canvas.getContext('2d');
 }
 
