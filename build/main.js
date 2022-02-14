625cd5c1c0d6048ce1479a7bafb943d2655918b5
Peter Horvath <horvthpeter@gmail.com>
2022-02-15 00:01:04 +0200

    Update

--- build/main.js
+++ build/main.js
@@ -1,8 +1,15 @@
+const FPS = 30;
+const SNAKES = new Map();
+SNAKES.set('0', [{ x: 100, y: 100 }]);
 function initGame() {
     const canvas = document.getElementById('canvas');
-    canvas.width = window.innerWidth - 2;
-    canvas.height = window.innerHeight - 2;
+    canvas.width = window.innerWidth - 6;
+    canvas.height = window.innerHeight - 6;
     const ctx = canvas.getContext('2d');
+    ctx.ellipse(300, 300, 20, 20, 0, 0, Math.PI * 2);
+    ctx.ellipse(310, 300, 20, 20, 0, 0, Math.PI * 2);
+    ctx.ellipse(320, 300, 20, 20, 0, 0, Math.PI * 2);
+    ctx.fill();
 }
 window.addEventListener('DOMContentLoaded', () => {
     initGame();
--- src/game.ts
+++ src/game.ts
@@ -1,10 +1,20 @@
+const FPS: number = 30;
+const SNAKES = new Map<string, Array<{x: number, y: number}>>();
+SNAKES.set('0', [{x: 100, y: 100}]);
+
 function initGame() {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
-    canvas.width = window.innerWidth - 2;
-    canvas.height = window.innerHeight - 2;
+    canvas.width = window.innerWidth - 6;
+    canvas.height = window.innerHeight - 6;
 
     const ctx = canvas.getContext('2d');
+
+    ctx.ellipse(300, 300, 20, 20, 0, 0, Math.PI * 2);
+    ctx.ellipse(310, 300, 20, 20, 0, 0, Math.PI * 2);
+    ctx.ellipse(320, 300, 20, 20, 0, 0, Math.PI * 2);
+
+    ctx.fill();
 }
 
 
